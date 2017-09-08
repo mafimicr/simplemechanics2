964e51a25b183afeca51d1df122467760cd795ee
Microne <mafimicr@isu.edu>
2017-09-07 19:59:55 -0600

    I wrote some codes

--- simplemechanics2/Form1.cs
+++ simplemechanics2/Form1.cs
@@ -16,5 +16,122 @@ namespace simplemechanics2
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int myNumber;
+            int anotherNumber;
+            int numberOfSteps;
+            int numberOfScores;
+            int numberOfCredits;
+            int numberOfPlayers;
+            int numberOfGoals;
+            int numberOfOrranges;
+            int numberOfSchools;
+            double averageScore;
+            double totalDistanceTraveled;
+            double testGrade;
+            double areaOfScore;
+            double minutesLeftInClass;
+            double appleidForce;
+            double zambiaPopulation;
+            double amountOfMoney;
+            double totalArea;
+            double weightLost;
+            double amoutOfGass;
+            string someText;
+            string firstName;
+            string firstAndLastName;
+            string fromFromFile;
+            string favoriteColor;
+            string mindleName;
+            string bestSoccerPlayer;
+            string testFruit;
+            string bestFriend;
+            string lagestSugarCompany;
+            string favoriteStoryBook;
+            bool isOdd;
+            bool isEven;
+            bool wasLeftOver;
+            bool isChecked;
+            bool isVailable;
+            bool isNamed;
+            bool isLeftOver;
+            bool failedTheTest;
+            bool passedTheExam;
+            bool isAddet;
+            bool isInterger;
+            bool isDouble;
+            bool isSelected;
+            int aNumber = 10;
+            int giveMeANumber = 43;
+            int stepsTaken = 5346;
+            int numberOfAgges = 681;
+            int numberOfStudents = 20;
+            int aGoals = 3;
+            int numberOfClasses = 8;
+            int numberOfCountries = 53;
+            int numberOfChairs = 358;
+            int numberOfBooks = 14;
+            double changeLeft = 34.5;
+            double productPrice = 1567.252;
+            double temperatureRightNow = 96.0;
+            double circumferenceOfCircle = 50.11;
+            double timeLeft = 18.6426;
+            double highestScore = 99.67;
+            double nomalBodyTemperature = 35.33;
+            double tatolDistanceCovered = 498.876;
+            double avarageScore = 76.887;
+            double minimumGradingPointAverage = 2.5;
+            double moneySpent = 488.89;
+            string myText = ("Hello");
+            string herTown = ("San Francisco");
+            string actorName = ("Mary Tyler Moore");
+            string theSayinng = ("Apple of my eye");
+            string favariteColor = ("blue");
+            string soccerLegend = ("Messi");
+            string favoriteMovie = ("fast and furious 8");
+            string bestNationalBasketAssociationPlayer = ("Michael Jordan");
+            string MyEnglishInstructor = ("Elis Barker");
+            string myFavoriteAfricanFood = ("Fufu");
+            string myHomeCountry = ("Zambia");
+            bool isLeft = true;
+            bool isRight = false;
+            bool isNotUp = false;
+            bool isToggled = false;
+            bool isVisible = false;
+            bool keepMoving = true;
+            bool wasStopped = false;
+            bool studentsPasses = true;
+            bool studentsFailed = false;
+            bool maximumHeight = true;
+            bool minimumLengeth = false;
+            bool isPicked = true;
+            bool isRegular = true;
+            int number1, number2;
+            int firstDay, SecondDay, thirdDay;
+            double price1, price2;
+            string startingText, endingText;
+            bool isOddRow, isEvenRow;
+            int countRend = 47, countBlue = 92;
+            int openBoxes = 153, returnedItems = 59, clearanceItems = 77;
+            double item1Quality = 99.6, item2Quality = 86.7;
+            string openimgPhrase = ("once upon a time,"), closingPhrase = ("Happily ever after");
+            bool isRich = false, isLiked = true;
+
+
+
+
+
+
+
+
+
+        }
     }
 }
